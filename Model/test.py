9f2e0b88b2bbcf5d3356fbbeae700f78b7791ce5
Bartlomiej Zgorzynski <b.zgorzynski@gmail.com>
2021-01-05 10:59:11 +0100

    Model upgrade

--- Model/test.py
+++ Model/test.py
@@ -1,24 +1,23 @@
 from text_preprocessing import preprocess_data
 from gensim.models.doc2vec import Doc2Vec
 from gensim.test.utils import common_texts
+import pprint
 
 import pandas as pd
-
-# load data
-path = 'Model/Data/wiki_data.csv'
-df = pd.read_csv(path, delimiter=';', usecols = ['url'])
 
 #load model
 model = Doc2Vec.load("Model/d2v.model")
 
-# Test
+# Load test data
 file_path = 'Model/Data/1.txt'
 f = open(file_path, "r")
 text = f.read()
 data = preprocess_data(text)
 
+# Inferr vector
 inferred_vector = model.infer_vector(data)
-n = model.docvecs.most_similar([inferred_vector], topn= 3)
-print(df.iloc[n[0][0]])
-print(df.iloc[n[1][0]])
-print(df.iloc[n[2][0]])
+n = model.docvecs.most_similar([inferred_vector], topn = 15)
+
+# Print output
+for n_ in n:
+    print(n_)
